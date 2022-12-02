b3dea98a410380c1920671acde76d299d6e8f41b
romario <romario151996@gmail.com>
2022-12-02 09:18:58 -0500

    init

---
+++ src/main/java/com/krugerchallenge/vaccinationback/VaccinationBackApplication.java
@@ -0,0 +1,13 @@
+package com.krugerchallenge.vaccinationback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaccinationBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaccinationBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krugerchallenge/vaccinationback/VaccinationBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krugerchallenge.vaccinationback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
